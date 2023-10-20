ea44f8d760ae63cd2bceeb058de3b433acce4230
Ephram Daniels <linguistnick7@gmail.com>
2023-10-20 13:47:42 +0200

    fixed some issues

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OwnerController;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
