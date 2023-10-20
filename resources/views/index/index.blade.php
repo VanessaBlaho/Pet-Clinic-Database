f0286afcf7817c6a250b788e80e018304fe184f6
Ephram Daniels <linguistnick7@gmail.com>
2023-10-20 11:43:51 +0200

    added index controller, index blade file, and index route

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Greatest Vet Clinic of All Time</title>
+</head>
+<body>
+    
+    <h1>The Greatest Vet Clinic of All Time</h1>
+
+    
+
+
+
+
+
+
+</body>
+</html>
