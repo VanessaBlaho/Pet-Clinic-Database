37d2ca2a103a23f2e315ec785df7fd6022ca9654
mariecotije <kkmarushka@gmail.com>
2023-10-20 11:50:55 +0200

    removed wrong method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,26 +10,4 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-
-    public function search()
-    {
-        $search_term = $_GET['search'] ?? null;
-
-        if ($search_term) {
-            $results = Animal::query()
-                ->where('name', 'like', '%' . $search_term . '%')
-                ->orderBy('name', 'asc')
-                ->get();
-        } else {
-            $results = Owner::query()
-                ->where('first_name', 'like', '%' . $search_term . '%')
-                ->where('surname', 'like', '%' . $search_term . '%')
-                ->orderBy('first_name', 'asc')
-                ->get();
-        }
-        return view('search.search', [
-            'search_term' => $search_term,
-            'results' => $results ?? []
-        ]);
-    }
 }
