9b1eb2768aaa657b3dc7b672b8decbc6e0a8d9d3
mariecotije <kkmarushka@gmail.com>
2023-10-20 11:54:39 +0200

    added search function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function search()
+    {
+        $search_term = $_GET['search'] ?? null;
+
+        if ($search_term) {
+            $results = Animal::query()
+                ->where('name', 'like', '%' . $search_term . '%')
+                ->orderBy('name', 'asc')
+                ->get();
+        } else {
+            $results = Owner::query()
+                ->where('first_name', 'like', '%' . $search_term . '%')
+                ->where('surname', 'like', '%' . $search_term . '%')
+                ->orderBy('first_name', 'asc')
+                ->get();
+        }
+        return view('search.search', [
+            'search_term' => $search_term,
+            'results' => $results ?? []
+        ]);
+    }
 }
